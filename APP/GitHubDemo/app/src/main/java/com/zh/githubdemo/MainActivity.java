dd450b3f58995a723923f1e1cabc1627ad47f062
is1988 <achoo@vip.qq.com>
2021-02-02 17:46:34 +0800

    项目本地仓库上传

---
+++ APP/GitHubDemo/app/src/main/java/com/zh/githubdemo/MainActivity.java
@@ -0,0 +1,23 @@
+package com.zh.githubdemo;
+
+
+import com.zh.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
